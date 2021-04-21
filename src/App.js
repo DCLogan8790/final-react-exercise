0f22124222b4657255900bd0b881759c97dba43a
Chelsea Logan <chelsealogan@SCWDBs-MBP.attlocal.net>
2021-04-21 09:26:51 -0500

    Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// 1a
+// 2a
 import MCUShows from './mcu-shows/MCUShows'
 
+// 5a
 const releaseDates = {
   wandaVision: 'Jan 2021',
   falconWinter: 'Mar 2021',
@@ -12,7 +13,9 @@ const releaseDates = {
 function App() {
   return (
     <div>
+      {/* 2b */}
       <h1>FINAL REACT EXERCISE</h1>
+      {/* 2c & 5b*/}
       <MCUShows dates={releaseDates} />
 
     </div>
--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -1,17 +1,22 @@
-
+// 1a
 import './MCUShows.css'
 
-const MCUShows = (releaseDates) => {
+// 1b
+const MCUShows = ({dates}) => {
+    // div>h1+h2+p*4
+    // 1c
     return (
         <div className= 'MCUShows'>
+            {/* 3b */}
            <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
            <h2>MCU Show Release Dates</h2>
-           <p>WandaVision: {releaseDates.wandaVision}</p>
-           <p>The Falcon and the Winter Soldier: </p>
-           <p>Loki:</p>
-           <p>Hawkeye:</p>
+           <p>WandaVision: {dates.wandaVision} </p>
+           <p>The Falcon and the Winter Soldier: {dates.falconWinter} </p>
+           <p>Loki: {dates.loki}</p>
+           <p>Hawkeye: {dates.hawkeye}</p>
         </div>
     );
 }
 
+// 1d
 export default MCUShows;
